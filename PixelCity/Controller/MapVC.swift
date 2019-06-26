a287bd887dd06aabe7e73b5b232f3a7dc8cacd69
James Volmert <james.volmert@gmail.com>
2019-06-26 14:25:45 -0600

    find current location and zoom in to street view

--- PixelCity/Controller/MapVC.swift
+++ PixelCity/Controller/MapVC.swift
@@ -8,21 +8,52 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
+    var locationManager = CLLocationManager()
+    let authStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    
     @IBOutlet weak var pixelMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         pixelMapView.delegate = self
-        
+        locationManager.delegate = self
+        configureLocationService()
     }
 
     @IBAction func mapButtonPressed(_ sender: Any) {
+        if authStatus == .authorizedAlways || authStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
     
 }
 
 extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        pixelMapView.setRegion(coordinateRegion, animated: true)
+        
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    
+    func configureLocationService() {
+        if authStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+    
     
 }
