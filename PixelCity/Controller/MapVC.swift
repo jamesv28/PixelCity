90c370b3732879b7ea9baf3abf4a9336e54d9d4a
James Volmert <james.volmert@gmail.com>
2019-07-08 15:56:49 -0600

    added 3d touch

--- PixelCity/Controller/MapVC.swift
+++ PixelCity/Controller/MapVC.swift
@@ -38,6 +38,7 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         photoCollectionView?.dataSource = self
         photoCollectionView?.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         
+        registerForPreviewing(with: self, sourceView: photoCollectionView!)
         galleryView.addSubview(photoCollectionView!)
         configureTileOverlay()
         
@@ -282,5 +283,23 @@ extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource {
         present(popVC, animated: true, completion: nil)
     }
     
+}
+
+extension MapVC: UIViewControllerPreviewingDelegate {
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        guard let indexPath = photoCollectionView?.indexPathForItem(at: location), let cell = photoCollectionView?.cellForItem(at: indexPath) else { return nil }
+        
+        guard let popVC = storyboard?.instantiateViewController(withIdentifier: "PopVC") as? PopVC else { return nil}
+        
+        popVC.initData(forImage: imageArray[indexPath.row])
+        
+        previewingContext.sourceRect = cell.contentView.frame
+        
+        return popVC
+    }
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+        show(viewControllerToCommit, sender: self)
+    }
 }
 
